27ecfc6930c8a542ebaacca95a710bfebab38691
John Chavez <70674723+eljohncena@users.noreply.github.com>
2026-07-18 00:40:10 -0400

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+
+        mapyMap = { ")" : "(" , "}" : "{" , "]" : "[" }
+
+        for character in s:
+            if character in mapyMap:
+                if stack and stack[-1] == mapyMap[character]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(character)
+
+        return True if not stack else False
